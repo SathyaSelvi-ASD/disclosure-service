0b72c7b7da34c32ecff547bd9c114a7d54487e38
Sathya ASD <sathya.selvi@ascendion.com>
2026-09-01 13:52:59 +0530

    Update test case and add new search api

--- src/main/java/com/vbox/disclosure/api/dto/request/DisclosureDto.java
+++ src/main/java/com/vbox/disclosure/api/dto/request/DisclosureDto.java
@@ -3,5 +3,5 @@ package com.vbox.disclosure.api.dto.request;
 import java.time.Instant;
 
 public record DisclosureDto(Long id, String referenceNumber, String customerId,
-                            String description, String status, Instant createdAt) {
+                            String description, String disclosureStatus, Instant createdAt) {
 }
